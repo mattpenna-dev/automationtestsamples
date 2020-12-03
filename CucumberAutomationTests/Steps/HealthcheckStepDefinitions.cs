c7b40b07350e5ccb05adc39e03584a1b3b1bb848
Victor Queliz <Victor.Queliz@insight.com>
2020-12-03 09:56:24 -0500

    Updated CommonSteps and Healthcheck files

---
+++ CucumberAutomationTests/Steps/HealthcheckStepDefinitions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Gherkin.Quick;
+
+namespace CucumberAutomationTests.Steps
+{
+    [FeatureFile("./Features/Healthcheck.feature")]
+    public sealed class HealthcheckStepDefinition : CommonStepDefinitions
+    {
+        private readonly HttpClient _httpClient = new HttpClient();
+        private HttpResponseMessage _httpResponseMessage;
+
+        [When(@"When I make a Get Car Service Health check to the Car Service")]
+        public async Task CallToTheHealthCheckEndpoint()
+        {
+            _httpResponseMessage = await _httpClient.GetAsync("https://ci-car-service.mattpenna.dev/actuator/health");
+            AddObject("HttpResponse", _httpResponseMessage);
+        }
+    }
+}
+}
