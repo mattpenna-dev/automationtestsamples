3f7bd0adf5fe41e47dde9d5970043891bf9c2fb5
Victor Queliz <Victor.Queliz@insight.com>
2020-12-03 09:39:40 -0500

    Updated Commonsteps

---
+++ CucumberAutomationTests/Steps/CommonStepDefinitions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit.Gherkin.Quick;
+
+namespace CucumberAutomationTests.Steps
+{
+    public abstract class CommonStepDefinitions : Feature
+    {
+        private Dictionary<string, object> _objects = new Dictionary<string, object>();
+
+        [Then(@"I should get an (.*) status")]
+        public void ThenIShouldGetAnStatus(int statusCode)
+        {
+            var httpResponseMessage = (HttpResponseMessage)GetObject("HttpResponse");
+            Assert.Equal(statusCode, (int)httpResponseMessage.StatusCode);
+        }
+
+        protected void AddObject(string key, object obj)
+        {
+            if (_objects.ContainsKey(key))
+            {
+                _objects[key] = obj;
+                return;
+            }
+
+            _objects.Add(key, obj);
+        }
+
+        protected object GetObject(string key)
+        {
+            if (_objects.ContainsKey(key))
+            {
+                return _objects[key];
+            }
+
+            throw new Exception("Key not found in objects");
+        }
+    }
+}
