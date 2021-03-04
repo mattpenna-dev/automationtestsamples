a8338579c2a8d44cf67a10418186d84ea32cebce
Matt Chau <mcchau92@gmail.com>
2021-03-04 12:55:59 -0500

    Add GetAll Step Definition

---
+++ CucumberAutomationTests/Steps/GetAllStepDefinition.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CucumberAutomationTests.Exceptions;
+using CucumberAutomationTests.Models.Car;
+using CucumberAutomationTests.Models.Manufacturer;
+using Newtonsoft.Json;
+using Xunit;
+using Xunit.Gherkin.Quick;
+
+namespace CucumberAutomationTests.Steps
+{
+    [FeatureFile("./Features/GetAll.feature")]
+    public class GetAllStepDefinition : CommonStepDefinition
+    {
+        private readonly HttpClient _httpClient;
+
+        public GetAllStepDefinition()
+        {
+            _httpClient = new HttpClient();
+        }
+
+    }
+}
